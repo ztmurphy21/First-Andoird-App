a1740557ac7817c9194602cc95d467dde03cc1de
Zachary Murphy <murphyz@my.wccc.edu>
2017-01-05 22:12:28 -0500

    buttons for maps and webpages.

--- Hour2Application/app/src/main/java/com/example/zacha/hour2application/MainActivity.java
+++ Hour2Application/app/src/main/java/com/example/zacha/hour2application/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.net.Uri;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //button for secondary activity
         Button activityButton = (Button) findViewById(R.id.button);
             activityButton.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -21,5 +25,33 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(startIntent);
             }
         });
+        //button for map use
+        Button mapButton =  (Button) findViewById(R.id.button2) ;
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String geoURI = "geo:37.422,-122.084?z=23";
+                Uri geo = Uri.parse(geoURI);
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, geo);
+                if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(mapIntent);
+                }
+            }
+        });
+        //button to launch webpage
+        Button webButton = (Button) findViewById(R.id.button3);
+        webButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View view){
+                String webURI = "http://www.google.com";
+                Uri web = Uri.parse(webURI);
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, web);
+                if (webIntent.resolveActivity(getPackageManager()) !=null){
+                    startActivity(webIntent);
+                }
+            }
+        });
     }
+
+
 }
